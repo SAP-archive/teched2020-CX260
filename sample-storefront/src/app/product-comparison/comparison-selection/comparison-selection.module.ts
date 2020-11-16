41a26346026af3a1ffef0985bed8c79ee9674eb9
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-11-16 21:25:27 +0100

    sample code for exercise 4

--- sample-storefront/src/app/product-comparison/comparison-selection/comparison-selection.module.ts
+++ sample-storefront/src/app/product-comparison/comparison-selection/comparison-selection.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ComparisonSelectionComponent } from './comparison-selection.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [ComparisonSelectionComponent],
 })
 export class ComparisonSelectionModule {}
