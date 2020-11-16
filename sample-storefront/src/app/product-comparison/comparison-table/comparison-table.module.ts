f779c6ad4f9e26e6db701363b38ab74fc43923e1
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-11-16 22:08:08 +0100

    sample code for exercise 7

--- sample-storefront/src/app/product-comparison/comparison-table/comparison-table.module.ts
+++ sample-storefront/src/app/product-comparison/comparison-table/comparison-table.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ComparisonFeaturesModule } from '../comparison-features/comparison-features.module';
 import { ComparisonHeaderModule } from '../comparison-header/comparison-header.module';
 import { ComparisonTableComponent } from './comparison-table.component';
 
 @NgModule({
   declarations: [ComparisonTableComponent],
-  imports: [CommonModule, ComparisonHeaderModule],
+  imports: [CommonModule, ComparisonHeaderModule, ComparisonFeaturesModule],
 })
 export class ComparisonTableModule {}
